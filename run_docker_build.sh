c07c902182d126f1a633bc6558bf622d01e0fac2
Julien Schueller <schueller@phimeca.com>
2018-10-24 14:35:46 +0200

    Missing dll

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -24,7 +24,6 @@ cd workdir
 git checkout ${VERSION}
 PREFIX=$PWD/install
 CXXFLAGS="-D_hypot=hypot -DNDEBUG" ${ARCH}-w64-mingw32-cmake \
-  -DBUILD_SHARED_LIBS=OFF \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib \
   -DPYTHON_INCLUDE_DIR=${MINGW_PREFIX}/include/python${PYMAJMIN} \
   -DPYTHON_LIBRARY=${MINGW_PREFIX}/lib/libpython${PYMAJMIN}.dll.a \
@@ -32,9 +31,11 @@ CXXFLAGS="-D_hypot=hypot -DNDEBUG" ${ARCH}-w64-mingw32-cmake \
   -DPYTHON_SITE_PACKAGES=Lib/site-packages \
     .
 make install
+${ARCH}-w64-mingw32-strip --strip-unneeded ${PREFIX}/bin/*.dll ${PREFIX}/Lib/site-packages/*/*.pyd
 
-${ARCH}-w64-mingw32-strip --strip-unneeded ${PREFIX}/Lib/site-packages/*/*.pyd
-cd ${PREFIX}/Lib/site-packages/ && zip -r /tmp/workdir/agrum-${VERSION}-py${PYBASEVER}-${ARCH}.zip * && cd -
+cp -v ${MINGW_PREFIX}/bin/{libgcc_s,libstdc++,libgomp,libwinpthread}*.dll ${PREFIX}/bin/*.dll ${PREFIX}/Lib/site-packages/pyAgrum
+
+cd ${PREFIX}/Lib/site-packages/ && zip -r /tmp/workdir/agrum-${VERSION}-py${PYBASEVER}-${ARCH}.zip pyAgrum && cd -
 
 if test -n "${uid}" -a -n "${gid}"
 then
