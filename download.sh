1ece321cf208303ecbf6f8e0ab48deeb13dfdc05
Julien Schueller <schueller@phimeca.com>
2018-10-24 21:07:26 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 release=0.13.2
+_arch=x86_64
 for pybasever in 2.7 3.6 3.7
 do
-  _file=agrum-py${pybasever}-${_arch}.zip
+  _file=agrum-${release}-py${pybasever}-${_arch}.zip
   wget -c https://github.com/openturns/build-agrum/releases/download/v${release}/${_file} -P /tmp
 done
 
