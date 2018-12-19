a94ceac217cb5e0722abfdf8fb0467f00d3ee9b3
Julien Schueller <schueller@phimeca.com>
2018-12-19 19:40:09 +0100

    Update to 0.13.4

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -18,12 +18,6 @@ MINGW_PREFIX=/usr/${ARCH}-w64-mingw32
 uid=$4
 gid=$5
 
-FOR_PYTHON3=ON
-if test "${PYBASEVER}" = "2.7"
-then
-  FOR_PYTHON3=OFF
-fi
-
 cd /tmp
 git clone https://gitlab.com/agrumery/aGrUM.git workdir
 cd workdir
@@ -31,13 +25,12 @@ git checkout ${VERSION}
 
 PREFIX=$PWD/install
 # https://gitlab.com/agrumery/aGrUM/issues/15
-CXXFLAGS="-DNDEBUG -O1" ${ARCH}-w64-mingw32-cmake \
+CXXFLAGS="-fno-strict-aliasing" ${ARCH}-w64-mingw32-cmake \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib \
   -DPYTHON_INCLUDE_DIR=${MINGW_PREFIX}/include/python${PYMAJMIN} \
   -DPYTHON_LIBRARY=${MINGW_PREFIX}/lib/libpython${PYMAJMIN}.dll.a \
   -DPYTHON_EXECUTABLE=/usr/bin/${ARCH}-w64-mingw32-python${PYMAJMIN}-bin \
-  -DPYTHON_SITE_PACKAGES=Lib/site-packages \
-  -DFOR_PYTHON3=${FOR_PYTHON3} \
+  -DPYTHON_INSTALL=Lib/site-packages \
   -DUSE_SWIG=OFF \
   .
 make install
@@ -48,7 +41,6 @@ cp -v ${MINGW_PREFIX}/bin/{libgcc_s,libstdc++,libgomp,libwinpthread}*.dll ${PREF
 
 touch numpy.py
 ${ARCH}-w64-mingw32-python${PYMAJMIN}-bin -c "import pyAgrum as gum; bn = gum.fastBN('a->b->d;a->c->d->e;f->b'); ie = gum.LazyPropagation(bn); ie.makeInference(); print(ie.posterior('d'))"
-${ARCH}-w64-mingw32-python${PYMAJMIN}-bin -c "import pyAgrum as gum; bn = gum.fastBN('a->b->d;a->c->d->e;f->b'); bn.saveDSL('two.dsl'); bn3 = gum.loadBN('two.dsl'); print(bn3)"
 
 zip -r agrum-${VERSION}-py${PYBASEVER}-${ARCH}.zip pyAgrum
 
